dc1c1deda722d1a76aedbf48dc09bf31cd88222e
Noah <noahleviwilder@gmail.com>
2019-04-01 21:21:07 -0400

    Added various testing methods to Testing class

--- Testing.java
+++ Testing.java
@@ -1,18 +1,62 @@
 package FST;
 
-
 //
 // Testing.java
 // ComputerScience
 //
-// Copyright © 2019 Noah Wilder. All rights reserved.
-// Last modified on 01/04/19 12:02 PM.
-//
- 
- public class Testing {
+// Copyright © 2019 Noah Wilder and Etan Ossip. All rights reserved.
+// Last modified on 01/04/19 2:22 PM.
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+
+public class Testing {
     
     public static void main(String[] args) {
         
     }
-    
+
+
+    // Testing method suite
+    public static void precondition(boolean condition) {
+        if (!condition) {
+            throw new AssertionError();
+        }
+    }
+    public static void precondition(Supplier<Boolean> condition) {
+        if (!condition.get()) {
+            throw new AssertionError();
+        }
+    }
+    public static void assertEqual(Object o1, Object o2) {
+        if (!Objects.equals(o1, o2)) {
+            throw new AssertionError();
+        }
+    }
+    public static void assertNotEqual(Object o1, Object o2) {
+        if (Objects.equals(o1, o2)) {
+            throw new AssertionError();
+        }
+    }
+    public static <T extends Comparable<U>, U> void assertLessThan(T o1, U o2) {
+        if (!(o1.compareTo(o2) < 0)) {
+            throw new AssertionError();
+        }
+    }
+    public static <T extends Comparable<U>, U> void assertGreaterThan(T o1, U o2) {
+        if (!(o1.compareTo(o2) > 0)) {
+            throw new AssertionError();
+        }
+    }
+    public static <T extends Comparable<U>, U> void assertLessThanOrEqual(T o1, U o2) {
+        if (!(o1.compareTo(o2) <= 0)) {
+            throw new AssertionError();
+        }
+    }
+    public static <T extends Comparable<U>, U> void assertGreaterThanOrEqual(T o1, U o2) {
+        if (!(o1.compareTo(o2) >= 0)) {
+            throw new AssertionError();
+        }
+    }
 }
